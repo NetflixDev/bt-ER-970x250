c829faca9f4d2a7b0f7c29a3426597035caedafe
Derek Miranda <derek.miranda@ff0000.com>
2019-04-12 14:34:47 -0700

    pkg: adds label
    - repub

--- dist/1-build/970x250/_c20-tracker/0367c5ab-f303-4457-9646-6eff86644817.js
+++ dist/1-build/970x250/_c20-tracker/0367c5ab-f303-4457-9646-6eff86644817.js
@@ -6,6 +6,7 @@ if (typeof module === 'undefined') {
 module.exports = {
   "id": "0367c5ab-f303-4457-9646-6eff86644817",
   "name": "bt-ER-970x250-right-corner",
+  "label": "Right Corner",
   "description": "C20/Monet-compatible templates that use  https://github.com/ff0000-ad-tech/ad-docs/blob/master/README.md",
   "type": "build_templates",
   "tier": "endframe-resolve",
@@ -14,7 +15,7 @@ module.exports = {
   "creative": "./3-traffic/default/970x250",
   "context": "RED Interactive",
   "state": "published",
-  "updated": 1554925484279,
+  "updated": 1555104807190,
   "username": "GitHub",
   "full_name": "NetflixDev/bt-ER-970x250-right-corner",
   "html_url": "https://github.com/NetflixDev/bt-ER-970x250-right-corner",
