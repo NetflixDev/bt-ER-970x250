475243547863207a7e7bc9677fa2bb0e3271f401
Derek Miranda <derek.miranda@ff0000.com>
2019-04-29 10:39:15 -0700

    pkg: renaming to just 970x250
    - repub

--- src/3-traffic/default/970x250/_c20-tracker/0367c5ab-f303-4457-9646-6eff86644817.js
+++ src/3-traffic/default/970x250/_c20-tracker/0367c5ab-f303-4457-9646-6eff86644817.js
@@ -5,7 +5,7 @@ if (typeof module === 'undefined') {
 // prettier-ignore
 module.exports = {
   "id": "0367c5ab-f303-4457-9646-6eff86644817",
-  "name": "bt-ER-970x250-right-corner",
+  "name": "bt-ER-970x250",
   "label": "Right Corner",
   "description": "C20/Monet-compatible templates that use  https://github.com/ff0000-ad-tech/ad-docs/blob/master/README.md",
   "type": "build_templates",
@@ -15,9 +15,9 @@ module.exports = {
   "creative": "./3-traffic/default/970x250",
   "context": "RED Interactive",
   "state": "published",
-  "updated": 1555104807190,
+  "updated": 1556559544538,
   "username": "GitHub",
-  "full_name": "NetflixDev/bt-ER-970x250-right-corner",
-  "html_url": "https://github.com/NetflixDev/bt-ER-970x250-right-corner",
+  "full_name": "NetflixDev/bt-ER-970x250",
+  "html_url": "https://github.com/NetflixDev/bt-ER-970x250",
   "version": "2.2.1"
 }
