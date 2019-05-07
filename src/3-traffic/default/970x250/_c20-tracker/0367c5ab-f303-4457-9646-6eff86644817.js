78ef4103160764ef040ad9ec75156615408634fa
Greg Connell <greg.connell@ff0000.com>
2019-05-06 17:20:17 -0700

    do not rely on @netflixadseng being installed

--- src/3-traffic/default/970x250/_c20-tracker/0367c5ab-f303-4457-9646-6eff86644817.js
+++ src/3-traffic/default/970x250/_c20-tracker/0367c5ab-f303-4457-9646-6eff86644817.js
@@ -26,7 +26,7 @@ module.exports = {
   },
   "context": "RED Interactive",
   "state": "published",
-  "updated": 1556844940823,
+  "updated": 1557188412644,
   "username": "GitHub",
   "full_name": "NetflixDev/bt-ER-970x250",
   "html_url": "https://github.com/NetflixDev/bt-ER-970x250",
