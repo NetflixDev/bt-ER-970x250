e3794df91890581c528c08861578a62065e89b2d
Derek Miranda <derek.miranda@ff0000.com>
2019-05-15 18:24:50 -0700

    feature: adds supercut option

--- src/3-traffic/default/970x250/_c20-tracker/0367c5ab-f303-4457-9646-6eff86644817.js
+++ src/3-traffic/default/970x250/_c20-tracker/0367c5ab-f303-4457-9646-6eff86644817.js
@@ -6,29 +6,29 @@ if (typeof module === 'undefined') {
 module.exports = {
   "id": "0367c5ab-f303-4457-9646-6eff86644817",
   "name": "bt-ER-970x250",
+  "label": "Right Corner",
   "description": "C20/Monet-compatible templates that use  https://github.com/ff0000-ad-tech/ad-docs/blob/master/README.md",
   "type": "build_templates",
   "builder": {
-    "label": "970x250 NF Global Template",
+    "label": "970x250",
     "tier": "endframe-resolve",
     "size": "970x250",
-    "creative": "./dist/3-traffic/default/970x250",
-    "toolkit": "./toolkit/970x250_NF_Global_Template_ContainerRecycle_Layered.psd",
+    "creative": "./3-traffic/default/970x250",
     "layouts": {
       "SIDE_BY_SIDE_LEFT": "Side-By-Side Left",
       "SIDE_BY_SIDE_OFF_CENTER_LEFT": "Side-By-Side Off-Center Left",
       "SIDE_BY_SIDE_CENTER": "Side-By-Side Center",
       "SIDE_BY_SIDE_OFF_CENTER_RIGHT": "Side-By-Side Off-Center Right",
       "SIDE_BY_SIDE_RIGHT": "Side-By-Side Right",
-      "STACKED": "Stacked"
+      "STACKED_LEFT": "Stacked Left",
+      "STACKED_CENTER": "Stacked Center"
     }
   },
   "context": "RED Interactive",
   "state": "published",
-  "updated": 1557791170892,
+  "updated": 1556665155645,
   "username": "GitHub",
   "full_name": "NetflixDev/bt-ER-970x250",
   "html_url": "https://github.com/NetflixDev/bt-ER-970x250",
-  "version": "2.2.1",
-  "minimum": "2.2.1"
+  "version": "2.2.1"
 }
