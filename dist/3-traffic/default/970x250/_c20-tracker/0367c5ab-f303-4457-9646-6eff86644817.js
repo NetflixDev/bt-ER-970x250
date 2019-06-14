3247ba6a92f8c40cbe19ff4f3870e79cefe31a8b
Derek Miranda <derek.miranda@ff0000.com>
2019-06-14 10:50:31 -0700

    chore: redist

--- dist/3-traffic/default/970x250/_c20-tracker/0367c5ab-f303-4457-9646-6eff86644817.js
+++ dist/3-traffic/default/970x250/_c20-tracker/0367c5ab-f303-4457-9646-6eff86644817.js
@@ -24,7 +24,7 @@ module.exports = {
   },
   "context": "RED Interactive",
   "state": "published",
-  "updated": 1559262990662,
+  "updated": 1560534615142,
   "username": "GitHub",
   "full_name": "NetflixDev/bt-ER-970x250",
   "html_url": "https://github.com/NetflixDev/bt-ER-970x250",
