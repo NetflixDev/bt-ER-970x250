b7eeb7d2d01b47a29e6617fa870ae1c63c759ff4
Derek Miranda <derek.miranda@ff0000.com>
2019-07-02 11:57:03 -0700

    cleans: removes TT images
    - repub

--- src/1-build/common/js/EndFrame/postmarkups/shared.js
+++ src/1-build/common/js/EndFrame/postmarkups/shared.js
@@ -1,162 +1,146 @@
-import { Styles, Markup, Align, Effects } from 'ad-view'
-import { ImageManager } from 'ad-control'
-import { Animation } from '@common/js/Animation.js'
-import { Control } from '@common/js/Control.js'
-import CanvasIris from '@common/js/CanvasIris.js'
-import { UIComponent, UIImage, TextFormat, UITextField, UIGroup } from 'ad-ui'
-import { ObjectUtils } from 'ad-utils'
+import { Styles, Clamp, Align } from "ad-view";
+import { ImageManager } from "ad-control";
+import { Animation } from "@common/js/Animation.js";
+import { Control } from "@common/js/Control.js";
+import CanvasIris from "@common/js/CanvasIris.js";
+import { UIComponent, UIImage, TextFormat, UITextField, UIGroup } from "ad-ui";
+import { ObjectUtils } from "ad-utils";
 
 export function titleTreatmentLayout(T) {
-	Align.set(T.tt, Align.CENTER)
+  Align.set(T.tt, Align.CENTER);
 }
 
-export function sideBySideBrandingLockup(T, { ctaLogoOffset, tuneInFontSize, tuneInLockupOffset, brandingLockupAlign }) {
-	// cta
-	T.cta.resize()
+function formatTextElToBrandingLockup(
+  textEl,
+  brandingLockup,
+  { textFontSize, textLockupOffset, textAlign = "center" } = {}
+) {
+  Styles.setCss(textEl, {
+    color: "#fff",
+    textAlign,
+    fontSize: textFontSize,
+    letterSpacing: 1
+  });
 
-	// logo
-	Align.set(T.netflixLogo, {
-		x: {
-			type: Align.LEFT,
-			outer: true,
-			against: T.cta,
-			offset: -ctaLogoOffset
-		}
-	})
+  // resize text element to current branding lockup's width
+  textEl.style.width = `${brandingLockup.offsetWidth}px`;
+  // set text element height equal to inner span's height
+  textEl.style.height = `${textEl.querySelector("span").offsetHeight}px`;
 
-	// lockup to position CTA and logo together
-	T.brandingLockup = new UIGroup({
-		target: T,
-		children: [T.cta, T.netflixLogo]
-	})
+  Align.set(textEl, {
+    x: { type: Align.CENTER, against: brandingLockup },
+    y: {
+      type: Align.TOP,
+      outer: true,
+      against: brandingLockup,
+      offset: -textLockupOffset
+    }
+  });
+}
 
-	if (adData.hasFTM) {
-		// free trial messaging
-		Styles.setCss(T.ftm, {
-			color: '#fff',
-			fontSize: tuneInFontSize - 2,
-			letterSpacing: 1,
-			textAlign: 'center'
-		})
-		Align.set(T.ftm, {
-			x: { type: Align.CENTER, against: T.brandingLockup },
-			y: {
-				type: Align.TOP,
-				outer: true,
-				against: T.brandingLockup,
-				offset: -tuneInLockupOffset
-			}
-		})
-		T.removeChild(T.tuneIn)
-	} else {
-		// tune-in
-		Styles.setCss(T.tuneIn, {
-			color: '#fff',
-			fontSize: tuneInFontSize,
-			letterSpacing: 1,
-			textAlign: 'center'
-		})
-		Align.set(T.tuneIn, {
-			x: { type: Align.CENTER, against: T.brandingLockup },
-			y: {
-				type: Align.TOP,
-				outer: true,
-				against: T.brandingLockup,
-				offset: -tuneInLockupOffset
-			}
-		})
-		T.removeChild(T.ftm)
-	}
+export function sideBySideBrandingLockup(
+  T,
+  { ctaLogoOffset, tuneInFontSize, tuneInLockupOffset, brandingLockupAlign }
+) {
+  // cta
+  T.cta.resize();
 
-	// add tune-in/ftm to lockup if exists
-	if (adData.hasTuneIn || adData.hasFTM) {
-		const children = [T.brandingLockup]
-		children.push(adData.hasTuneIn ? T.tuneIn : T.ftm)
+  // logo
+  Align.set(T.netflixLogo, {
+    x: {
+      type: Align.LEFT,
+      outer: true,
+      against: T.cta,
+      offset: -ctaLogoOffset
+    }
+  });
 
-		T.brandingLockup = new UIGroup({
-			target: T,
-			children
-		})
-	}
+  // lockup to position CTA and logo together
+  T.brandingLockup = new UIGroup({
+    target: T,
+    children: [T.cta, T.netflixLogo]
+  });
 
-	Align.set(T.brandingLockup, brandingLockupAlign)
+  // add tune-in/ftm to lockup if exists
+  if (adData.hasTuneIn || adData.hasFTM) {
+    const children = [T.brandingLockup];
+    const textEl = adData.hasTuneIn ? T.tuneIn : T.ftm;
+
+    if (adData.hasTuneIn) {
+      T.removeChild(T.ftm);
+    } else {
+      T.removeChild(T.tuneIn);
+    }
+
+    formatTextElToBrandingLockup(textEl, T.brandingLockup, {
+      textFontSize: adData.hasTuneIn ? tuneInFontSize : tuneInFontSize - 2,
+      textLockupOffset: tuneInLockupOffset
+    });
+
+    children.push(textEl);
+
+    T.brandingLockup = new UIGroup({
+      target: T,
+      children
+    });
+  }
+
+  Align.set(T.brandingLockup, brandingLockupAlign);
 }
 
 export function stackedBrandingLockup(
-	T,
-	{
-		// offset between elements in branding lockup
-		brandingLockupOffset,
-		// describe how to align branding lockup elems against each other
-		brandingLockupElemXAlign,
-		// Align value for entire branding lockup
-		brandingLockupAlign,
-		tuneInFontSize
-	}
+  T,
+  {
+    // offset between elements in branding lockup
+    brandingLockupOffset,
+    // describe how to align branding lockup elems against each other
+    brandingLockupElemXAlign,
+    // Align value for entire branding lockup
+    brandingLockupAlign,
+    tuneInFontSize
+  }
 ) {
-	// positioning CTA atop logo
-	T.cta.resize()
-	Align.set(T.cta, {
-		against: T.netflixLogo,
-		x: brandingLockupElemXAlign,
-		y: {
-			type: Align.TOP,
-			outer: true,
-			offset: -brandingLockupOffset
-		}
-	})
+  // positioning CTA atop logo
+  T.cta.resize();
+  Align.set(T.cta, {
+    against: T.netflixLogo,
+    x: brandingLockupElemXAlign,
+    y: {
+      type: Align.TOP,
+      outer: true,
+      offset: -brandingLockupOffset
+    }
+  });
 
-	const xAlignMatch = /((?:left)|(?:center)|(?:right))/i.exec(brandingLockupElemXAlign)
-	const textAlign = xAlignMatch && xAlignMatch[1].toLowerCase()
+  const xAlignMatch = /((?:left)|(?:center)|(?:right))/i.exec(
+    brandingLockupElemXAlign
+  );
+  const textAlign = xAlignMatch && xAlignMatch[1].toLowerCase();
 
-	// positioning tune-in/FTM atop CTA
-	if (adData.hasFTM) {
-		// free trial messaging
-		Styles.setCss(T.ftm, {
-			color: '#fff',
-			fontSize: tuneInFontSize - 2,
-			letterSpacing: 1,
-			textAlign
-		})
-		Align.set(T.ftm, {
-			x: { type: brandingLockupElemXAlign, against: T.cta },
-			y: {
-				type: Align.TOP,
-				outer: true,
-				against: T.cta,
-				offset: -(brandingLockupOffset - 4)
-			}
-		})
-		T.removeChild(T.tuneIn)
-	} else {
-		// tune-in
-		Styles.setCss(T.tuneIn, {
-			color: '#fff',
-			fontSize: tuneInFontSize,
-			letterSpacing: 1,
-			textAlign
-		})
-		Align.set(T.tuneIn, {
-			x: { type: brandingLockupElemXAlign, against: T.cta },
-			y: {
-				type: Align.TOP,
-				outer: true,
-				against: T.cta,
-				offset: -(brandingLockupOffset - 4)
-			}
-		})
-		T.removeChild(T.ftm)
-	}
+  // lockup to position branding elems together
+  const children = [T.cta, T.netflixLogo];
 
-	// lockup to position branding elems together
-	const children = [T.cta, T.netflixLogo]
+  if (adData.hasTuneIn || adData.hasFTM) {
+    const textEl = adData.hasTuneIn ? T.tuneIn : T.ftm;
 
-	if (adData.hasFTM) children.push(T.ftm)
-	if (adData.hasTuneIn) children.push(T.tuneIn)
+    if (adData.hasTuneIn) {
+      T.removeChild(T.ftm);
+    } else {
+      T.removeChild(T.tuneIn);
+    }
 
-	T.brandingLockup = new UIGroup({
-		target: T,
-		children,
-		align: brandingLockupAlign
-	})
+    formatTextElToBrandingLockup(textEl, T.cta, {
+      textAlign,
+      textFontSize: adData.hasTuneIn ? tuneInFontSize : tuneInFontSize - 2,
+      textLockupOffset: brandingLockupOffset - 4
+    });
+
+    children.push(textEl);
+  }
+
+  T.brandingLockup = new UIGroup({
+    target: T,
+    children,
+    align: brandingLockupAlign
+  });
 }
--- src/3-traffic/default/970x250/_c20-tracker/0367c5ab-f303-4457-9646-6eff86644817.js
+++ src/3-traffic/default/970x250/_c20-tracker/0367c5ab-f303-4457-9646-6eff86644817.js
@@ -1,6 +1,6 @@
 // GENERIC SOURCE TRACKER: bt-Endframe-Resolve
-if (typeof module === 'undefined') {
-	module = {}
+if (typeof module === "undefined") {
+  module = {};
 }
 // prettier-ignore
 module.exports = {
@@ -24,7 +24,7 @@ module.exports = {
   },
   "context": "RED Interactive",
   "state": "published",
-  "updated": 1561683488526,
+  "updated": 1562093799740,
   "username": "GitHub",
   "full_name": "NetflixDev/bt-ER-970x250",
   "html_url": "https://github.com/NetflixDev/bt-ER-970x250",
