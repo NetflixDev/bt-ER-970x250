c486a94192e806cb972bf4710de0d913f37cb296
Derek Miranda <derek.miranda@ff0000.com>
2019-07-31 17:01:51 -0700

    fix: changes C2.0 Retina field to Double Density
    - redist

--- dist/1-build/970x250/_c20-tracker/0367c5ab-f303-4457-9646-6eff86644817.js
+++ dist/1-build/970x250/_c20-tracker/0367c5ab-f303-4457-9646-6eff86644817.js
@@ -24,7 +24,7 @@ module.exports = {
   },
   "context": "RED Interactive",
   "state": "published",
-  "updated": 1564164140723,
+  "updated": 1564617543063,
   "username": "GitHub",
   "full_name": "NetflixDev/bt-ER-970x250",
   "html_url": "https://github.com/NetflixDev/bt-ER-970x250",
