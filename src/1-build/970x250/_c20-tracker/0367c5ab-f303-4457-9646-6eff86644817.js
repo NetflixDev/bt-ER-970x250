e142399cd44a8a89c651551751c591098c9c877d
Derek Miranda <derek.miranda@ff0000.com>
2019-08-14 12:07:38 -0700

    npm: C2.0 BT redist

--- src/1-build/970x250/_c20-tracker/0367c5ab-f303-4457-9646-6eff86644817.js
+++ src/1-build/970x250/_c20-tracker/0367c5ab-f303-4457-9646-6eff86644817.js
@@ -9,7 +9,6 @@ module.exports = {
   "description": "C20/Monet-compatible templates that use  https://github.com/ff0000-ad-tech/ad-docs/blob/master/README.md",
   "type": "build_templates",
   "builder": {
-    "label": "970x250 NF Global Template",
     "tier": "endframe-resolve",
     "size": "970x250",
     "creative": "./dist/3-traffic/default/970x250",
@@ -25,7 +24,7 @@ module.exports = {
   },
   "context": "RED Interactive",
   "state": "published",
-  "updated": 1557970180125,
+  "updated": 1565809634004,
   "username": "GitHub",
   "full_name": "NetflixDev/bt-ER-970x250",
   "html_url": "https://github.com/NetflixDev/bt-ER-970x250",
