67d0c6b9bce6b54d573eb2b8c55c2509e6b63db7
Derek Miranda <derek.miranda@ff0000.com>
2019-09-09 21:48:19 -0700

    Mobile intro zoom fix

--- dist/3-traffic/default/970x250/_c20-tracker/0367c5ab-f303-4457-9646-6eff86644817.js
+++ dist/3-traffic/default/970x250/_c20-tracker/0367c5ab-f303-4457-9646-6eff86644817.js
@@ -24,7 +24,7 @@ module.exports = {
   },
   "context": "RED Interactive",
   "state": "published",
-  "updated": 1568054146310,
+  "updated": 1568090883060,
   "username": "GitHub",
   "full_name": "NetflixDev/bt-ER-970x250",
   "html_url": "https://github.com/NetflixDev/bt-ER-970x250",
