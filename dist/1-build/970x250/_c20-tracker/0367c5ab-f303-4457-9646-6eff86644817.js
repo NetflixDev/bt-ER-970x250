d3de374d7783d12b39698c2987f9e700c51a56c2
Derek Miranda <derek.miranda@ff0000.com>
2019-09-30 15:58:38 -0700

    Applies any remaining layout fixes
    - redist

--- dist/1-build/970x250/_c20-tracker/0367c5ab-f303-4457-9646-6eff86644817.js
+++ dist/1-build/970x250/_c20-tracker/0367c5ab-f303-4457-9646-6eff86644817.js
@@ -1,6 +1,6 @@
 // GENERIC SOURCE TRACKER: bt-Endframe-Resolve
-if (typeof module === "undefined") {
-  module = {};
+if (typeof module === 'undefined') {
+	module = {}
 }
 // prettier-ignore
 module.exports = {
@@ -24,7 +24,7 @@ module.exports = {
   },
   "context": "RED Interactive",
   "state": "published",
-  "updated": 1569460238713,
+  "updated": 1569884288812,
   "username": "GitHub",
   "full_name": "NetflixDev/bt-ER-970x250",
   "html_url": "https://github.com/NetflixDev/bt-ER-970x250",
--- dist/1-build/common/js/EndFrame/inits/index.js
+++ dist/1-build/common/js/EndFrame/inits/index.js
@@ -1,36 +1,23 @@
-import { Styles, Markup, Align, Effects } from "ad-view";
-import { ImageManager } from "ad-control";
-import { Animation } from "@common/js/Animation.js";
-import { Control } from "@common/js/Control.js";
-import CanvasIris from "@common/js/CanvasIris.js";
-import {
-  UIComponent,
-  UIBorder,
-  UIButton,
-  UIImage,
-  TextFormat,
-  UITextField,
-  UISvg
-} from "ad-ui";
-import { ObjectUtils } from "ad-utils";
-import baseInit from "./baseInit.js";
+import { Styles, Markup, Align, Effects } from 'ad-view'
+import { ImageManager } from 'ad-control'
+import { Animation } from '@common/js/Animation.js'
+import { Control } from '@common/js/Control.js'
+import CanvasIris from '@common/js/CanvasIris.js'
+import { UIComponent, UIBorder, UIButton, UIImage, TextFormat, UITextField, UISvg } from 'ad-ui'
+import { ObjectUtils } from 'ad-utils'
+import baseInit from './baseInit.js'
 
-export { mainInit, stackedInit };
+export { mainInit, stackedInit }
 
 function mainInit(T) {
-  baseInit(T, {
-    logoWidth: 111,
-    ctaWidth: 111,
-    ctaMaxWidth: 130,
-    ctaHeight: 28
-  });
+	baseInit(T, { logoWidth: 111, ctaWidth: 111, ctaMaxWidth: 130, ctaHeight: 28 })
 }
 
 function stackedInit(T) {
-  baseInit(T, {
-    logoWidth: 121,
-    ctaWidth: 121,
-    ctaMaxWidth: 121,
-    ctaHeight: 32
-  });
+	baseInit(T, {
+		logoWidth: 121,
+		ctaWidth: 121,
+		ctaMaxWidth: 121,
+		ctaHeight: 32
+	})
 }
