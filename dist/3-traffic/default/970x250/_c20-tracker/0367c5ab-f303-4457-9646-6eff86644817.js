c9499045e864b0de5367baaaba8416d6f678e120
Derek Miranda <derek.miranda@ff0000.com>
2019-10-16 12:14:31 -0700

    Reinstalling components

--- dist/3-traffic/default/970x250/_c20-tracker/0367c5ab-f303-4457-9646-6eff86644817.js
+++ dist/3-traffic/default/970x250/_c20-tracker/0367c5ab-f303-4457-9646-6eff86644817.js
@@ -24,7 +24,7 @@ module.exports = {
   },
   "context": "RED Interactive",
   "state": "published",
-  "updated": 1571250240703,
+  "updated": 1571253263745,
   "username": "GitHub",
   "full_name": "NetflixDev/bt-ER-970x250",
   "html_url": "https://github.com/NetflixDev/bt-ER-970x250",
