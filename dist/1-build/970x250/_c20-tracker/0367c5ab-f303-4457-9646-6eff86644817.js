146db00fec563eb7c24418a28eab6232692602ad
Derek Miranda <derek.miranda@ff0000.com>
2019-12-12 13:43:50 -0800

    Applies FAT refactor changes
    - redist

--- dist/1-build/970x250/_c20-tracker/0367c5ab-f303-4457-9646-6eff86644817.js
+++ dist/1-build/970x250/_c20-tracker/0367c5ab-f303-4457-9646-6eff86644817.js
@@ -24,7 +24,7 @@ module.exports = {
   },
   "context": "RED Interactive",
   "state": "published",
-  "updated": 1571264601876,
+  "updated": 1576187002292,
   "username": "GitHub",
   "full_name": "NetflixDev/bt-ER-970x250",
   "html_url": "https://github.com/NetflixDev/bt-ER-970x250",
